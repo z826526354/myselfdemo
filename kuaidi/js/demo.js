d52fedbfe8d68a7e086a310be73b87d45a07f0ae
Tommy Chen <tommy351@gmail.com>
2019-08-29 21:29:12 +0800

    kuaidi

--- kuaidi/js/demo.js
+++ kuaidi/js/demo.js
@@ -26,105 +26,105 @@ function ajaxFun(method, url, data, callback, flag) {
     }else {
         xhr = new ActiveXObject('Microsoft.XMLHttp')
                 // 兼容IE
-            }
-            
-            method = method.toUpperCase();
-            // 兼容get/post大小写
-            if (method == 'GET') {
-                xhr.open('GET', url + '?' + data, flag);
-                xhr.send();
-            }else if (method == 'POST') {
-                xhr.open('POST', url, flag);
-                xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-                xhr.send(data);
-                // 兼容 GET 和 POST 请求
-            }
-            
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        callback(xhr.responseText);
-                    }
-                }
+    }
+    
+    method = method.toUpperCase();
+    // 兼容get/post大小写
+    if (method == 'GET') {
+        xhr.open('GET', url + '?' + data, flag);
+        xhr.send();
+    }else if (method == 'POST') {
+        xhr.open('POST', url, flag);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+        xhr.send(data);
+        // 兼容 GET 和 POST 请求
+    }
+    
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                callback(xhr.responseText);
             }
         }
-        
-        
-        getName.onchange = function () {
-        	if (locked % 2 != 1) {
-        		str = '';
-              locked ++;
-          }
-      }
-      phoneNum.onchange = function () {
-          if (locked % 2 != 1) {
-              str = '';
-              locked ++;
-          }
-      }
-      address.onchange = function () {
-          if (locked % 2 != 1) {
-              str = '';
-              locked ++;
-          }
-      }
-      wechat.onchange = function () {
-          if (locked % 2 != 1) {
-              str = '';
-              locked ++;
-          }
-      }
-      timee.onchange = function () {
-          if (locked % 2 != 1) {
-              str = '';
-              locked ++;
-          }
-      }
-      clas.onselect = function () {
-          if (locked % 2 != 1) {
-              str = '';
-              locked ++;
-          }
-      }
-      concret.onchange = function () {
-          if (locked % 2 != 1) {
-              str = '';
-              locked ++;
-          }
-      }
-      
-
-
-      money.onclick = function (e) {
-        e.preventDefault();
-        var data = 'getName=' + getName.value + '&phoneNum=' + phoneNum.value + '&address=' + address.value + '&wechat=' + wechat.value + '&timee=' + timee.value + '&clas=' + clas.value + '&concret=' + concret.value ;
-
-        ajaxFun('POST', './data/post.php', data, showPerson, true);
-        
     }
-
-
-    function showPerson(data) {
-        arr = new Set(data.split(','))
-        newArr = [...arr];
-        if (locked % 2 == 1) {
-           newArr.forEach(function(ele){
-              str += '<li class="list">' + ele + '</li>';
-              strcopy += ' ' + ele + ';'
-          })
-           locked++;
-       }
-       wrapUl.style.opacity = 1;
-       wrapUl.innerHTML = str;
-       copylike.innerHTML = strcopy;
-   }
-   btn.onclick = function () {
-       if (locked % 2 != 1) {
-          console.log(newArr);
-          copylike.select();
-          document.execCommand("Copy");
-          alert('复制成功, 赶快联系管理员估价吧')
-      }
-      locked++;
-      
+}
+        
+        
+getName.onchange = function () {
+	if (locked % 2 != 1) {
+		str = '';
+      locked ++;
   }
+}
+phoneNum.onchange = function () {
+  if (locked % 2 != 1) {
+      str = '';
+      locked ++;
+  }
+}
+address.onchange = function () {
+  if (locked % 2 != 1) {
+      str = '';
+      locked ++;
+  }
+}
+wechat.onchange = function () {
+  if (locked % 2 != 1) {
+      str = '';
+      locked ++;
+  }
+}
+timee.onchange = function () {
+  if (locked % 2 != 1) {
+      str = '';
+      locked ++;
+  }
+}
+clas.onselect = function () {
+  if (locked % 2 != 1) {
+      str = '';
+      locked ++;
+  }
+}
+concret.onchange = function () {
+  if (locked % 2 != 1) {
+      str = '';
+      locked ++;
+  }
+}
+
+
+
+money.onclick = function (e) {
+    e.preventDefault();
+    var data = 'getName=' + getName.value + '&phoneNum=' + phoneNum.value + '&address=' + address.value + '&wechat=' + wechat.value + '&timee=' + timee.value + '&clas=' + clas.value + '&concret=' + concret.value ;
+
+    ajaxFun('POST', './data/post.php', data, showPerson, true);
+
+}
+
+
+function showPerson(data) {
+    arr = new Set(data.split(','))
+    newArr = [...arr];
+    if (locked % 2 == 1) {
+        newArr.forEach(function(ele){
+            str += '<li class="list">' + ele + '</li>';
+            strcopy += ' ' + ele + ';'
+        })
+        locked++;
+    }
+    wrapUl.style.opacity = 1;
+    wrapUl.innerHTML = str;
+    copylike.innerHTML = strcopy;
+}
+btn.onclick = function () {
+        if (locked % 2 != 1) {
+        console.log(newArr);
+        copylike.select();
+        document.execCommand("Copy");
+        alert('复制成功, 赶快联系管理员估价吧')
+    }
+    locked++;
+
+}
